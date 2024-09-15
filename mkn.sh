567e69a2c7ea49898a2e1904d943130675728fc6
PhilipDeegan <philip.deegan@gmail.com>
2024-09-15 21:08:03 +0200

    ++

--- mkn.sh
+++ mkn.sh
@@ -6,11 +6,15 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     cd $CWD
     mkn -C mkn.base.yaml
     MKN_REPO=$(mkn -G MKN_REPO -C mkn.base.yaml)
-    rm -rf build && mkdir build && cd build
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
+    rm -rf build && mkdir build
+    (
+        cd build
+        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
                    -DEigen3_DIR="${MKN_REPO}/math/eigen/master/share/eigen3/cmake" \
                    -DBoost_DIR="${MKN_REPO}/org/boost/master/lib/cmake/Boost-1.86.0" \
                    -DFLANN_ROOT="${MKN_REPO}/math/flann/master" \
                    -DCMAKE_INSTALL_PREFIX=$CWD ../p
-    ninja && ninja install
+        ninja && ninja install
+    )
+    rm -rf build
 )
